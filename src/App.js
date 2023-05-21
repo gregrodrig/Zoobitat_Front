680ff8d17bc3108f3369171096cccb4266942b0f
greg3391 <gregory3391@gmail.com>
2023-05-21 15:51:47 +0200

    initial values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>HEADER</h1>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={""} />
+          <Route path="*" element={""} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* PROTEGIDAS */}
+          <Route element={<ProtectedRoute isAllowed={""} redirectTo="/" />}>
+            {/* RUTAS PROTEGIDAS */}
+          </Route>
+        </Routes>
+      </main>
     </div>
   );
 }
