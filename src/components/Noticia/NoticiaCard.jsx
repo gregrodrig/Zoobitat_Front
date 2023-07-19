ae84a9e68a594c19ff58ad611eb48d27676a38c8
greg3391 <gregory3391@gmail.com>
2023-07-19 14:51:34 +0200

    solving home noticia section responsive

--- src/components/Noticia/NoticiaCard.jsx
+++ src/components/Noticia/NoticiaCard.jsx
@@ -8,44 +8,49 @@ export default class NoticiaCard extends Component {
     const { imagen, titulo, texto, id } = this.props;
 
     return (
-      <Card className="mb-2" style={{ borderColor: "var(--LightGreen)" }}>
-        <Card.Body>
-          <Row>
-            <Col xs={4}>
-              <Link to={`/Noticia/${id}`}>
-                <Card.Img
-                  src={imagen}
-                  alt="Imagen de la noticia"
-                  style={{ width: "100%", height: "auto" }}
-                />
-              </Link>
-            </Col>
-            <Col xs={8}>
-              <Link to={`/Noticia/${id}`}>
-                <Card.Title
-                  style={{ color: "var(--MediumGreen)", textAlign: "start" }}
-                >
-                  {titulo}
-                </Card.Title>
-                <Card.Text
-                  style={{
-                    color: "var(--Black)",
-                    display: "-webkit-box",
-                    WebkitLineClamp: 2,
-                    WebkitBoxOrient: "vertical",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    maxWidth: "100%",
-                    margin: 0,
-                  }}
-                >
-                  {texto}
-                </Card.Text>
-              </Link>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+      <Col xs={12} md={6} lg={4}>
+        <Card className="mb-2" style={{ borderColor: "var(--LightGreen)" }}>
+          <Card.Body>
+            <Row>
+              <Col xs={4}>
+                <Link to={`/Noticia/${id}`}>
+                  <Card.Img
+                    src={imagen}
+                    alt="Imagen de la noticia"
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                </Link>
+              </Col>
+              <Col xs={8}>
+                <Link to={`/Noticia/${id}`}>
+                  <Card.Title
+                    style={{
+                      color: "var(--MediumGreen)",
+                      textAlign: "start",
+                    }}
+                  >
+                    {titulo}
+                  </Card.Title>
+                  <Card.Text
+                    style={{
+                      color: "var(--Black)",
+                      display: "-webkit-box",
+                      WebkitLineClamp: 2,
+                      WebkitBoxOrient: "vertical",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      maxWidth: "100%",
+                      margin: 0,
+                    }}
+                  >
+                    {texto}
+                  </Card.Text>
+                </Link>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
     );
   }
 }
