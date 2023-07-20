c1b731ac5e22a0338a306ea81563d63ddec1420f
greg3391 <gregory3391@gmail.com>
2023-07-20 18:05:39 +0200

    generating android local app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.zoobitat.app',
-  appName: 'zooobitat',
+  appName: 'zoobitat-app',
   webDir: 'build',
   server: {
     androidScheme: 'https'
